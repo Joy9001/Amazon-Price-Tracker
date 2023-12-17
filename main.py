3293acb1c93ca8264004776f95231dd2f75b163c
Joy <joymridha939@gmail.com>
2023-12-17 14:13:18 +0530

    Error Handled and Taking Input

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 import smtplib
 import os
 
-BASE_PRICE = 2000
+BASE_PRICE = int(input("Enter The Base Price: "))
 
 my_gmail = os.environ.get("YOUR_GMAIL")
 my_outlook = os.environ.get("YOUR_OUTLOOK")
@@ -14,23 +14,33 @@ headers = {
     "Accept-Language": "en-IN,en;q=0.9"
 }
 
-link = "https://www.amazon.in/Redragon-KUMARA-Backlit-Mechanical-Keyboard/dp/B016MAK38U/ref=sr_1_4?keywords=best%2Bmechanical%2Bkeyboard&sr=8-4&th=1"
+link = input("Enter your Product Link From Amazon: \n")
 
-response = requests.get(url=link, headers=headers).text
+try:
+    response = requests.get(url=link, headers=headers).text
+    soup = BeautifulSoup(response, "html.parser")
 
-# print(response)
-soup = BeautifulSoup(response, "html.parser")
+    get_price = soup.select_one(selector="span span .a-price-whole").getText()
+    price = float(get_price.split(".")[0].replace(',', ''))
 
-get_price = soup.select_one(selector="span span .a-price-whole").getText()
+    if price < BASE_PRICE:
+        message = f"Subject: AMAZON PRICE DROP ALART \n\n Redragon Kumara K552 Rainbow LED Backlit TKL Ten Key-Less Mechanical Wired Gaming Keyboard Without Numlock Keys (Black) is NOW {price}! \n!!! BUY NOW !!! \n{link}"
 
-price = float(get_price.split(".")[0].replace(',', ''))
+        smtp_server = "smtp.gmail.com"
+        try:
+            server = smtplib.SMTP(host=smtp_server, port=587)
+            server.starttls()
+            server.login(user=my_gmail, password=password_gmail)
+            server.sendmail(from_addr=my_gmail, to_addrs=my_outlook, msg=message)
+            print("Email sent successfully!")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    else:
+        print("Price Hasn't Decreased! I'll Notify When Found A Lower Price Than The Base Price!\n")
 
-if price < BASE_PRICE:
-    message = f"Subject: AMAZON PRICE DROP ALART \n\n Redragon Kumara K552 Rainbow LED Backlit TKL Ten Key-Less Mechanical Wired Gaming Keyboard Without Numlock Keys (Black) is NOW {price}! \n!!! BUY NOW !!! \n{link}"
-
-    smtp_server = "smtp.gmail.com"
-    server = smtplib.SMTP(host=smtp_server, port=587)
-    server.starttls()
-    server.login(user=my_gmail, password=password_gmail)
-    server.sendmail(from_addr=my_gmail, to_addrs=my_outlook, msg=f"{message}")
-    print("Email Sent Successfully.")
+except requests.exceptions.MissingSchema:
+    print("Invalid URL! You missed 'https://...' ")
+except AttributeError:
+    print("Please Enter A Valid Link From Amazon!")
+except requests.exceptions.InvalidSchema:
+    print("Please Enter A Valid Link ! No connection adapters were found!")
